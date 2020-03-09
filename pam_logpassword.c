c40af59ce29978167713d477432ddeda8c360496
guffre <mtguffr@gmail.com>
2020-03-08 20:08:15 -0400

    Update pam_logpassword.c

--- pam_logpassword.c
+++ pam_logpassword.c
@@ -10,14 +10,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <sys/file.h>
 #include <sys/time.h>
 #include <sqlite3.h>
 
